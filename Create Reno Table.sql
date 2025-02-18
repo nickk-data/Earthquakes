f25c4cd1c2e9f1408491d0e535054e7ad409956e
Nick K. <70929043+nickk-data@users.noreply.github.com>
2025-02-18 08:07:14 -0800

    Update Create Reno Table.sql
    added insert_date_time field and updated existing records to reflect when they were added to the table

--- Create Reno Table.sql
+++ Create Reno Table.sql
@@ -1,3 +1,4 @@
+-- Original 'create table'
 CREATE TABLE Reno (
 	quake_id SERIAL PRIMARY KEY NOT NULL,
 	time TIMESTAMP NOT NULL,
@@ -7,3 +8,17 @@ CREATE TABLE Reno (
 	magnitude decimal(10,3) NOT NULL,
 	place varchar(255) NOT NULL
 );
+
+-- 'Alter table' when I realized I didn't include an 'insert date time' field
+ALTER TABLE reno
+ADD insert_date_time TIMESTAMP;
+
+-- Inserted the date & time of the first run
+UPDATE reno
+SET insert_date_time = '2025-02-10 11:00:00.000' WHERE time <= '2025-02-10'; 
+
+-- Inserted the date & time of the second run
+UPDATE reno
+SET insert_date_time = '2025-02-18 07:55:00.000' WHERE time >= '2025-02-10';
+
+-- Table is now up to date
