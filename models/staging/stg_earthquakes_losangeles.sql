3dce95ca558f337e9e7ded85929c0a620c014b12
Nick Knowles <nick.knowles529@gmail.com>
2025-10-14 21:30:09 +0000

    converted time from utc 24 hr to pacific 12 hour. i did this since all locations in this project are in the pacific time zone. if this had multiple time zones, i would have left it at utc 24 hr.

--- models/staging/stg_earthquakes_losangeles.sql
+++ models/staging/stg_earthquakes_losangeles.sql
@@ -1,5 +1,29 @@
-select
-    *,
-    regexp_substr(place, 'of (.*)', 1, 1, 'i', 1) as nearest_municipality
+with source as (
+    select * 
+    from {{ source('earthquakes', 'LosAngeles') }} 
+),
 
-from {{ source('earthquakes', 'LosAngeles') }}
+convert_to_pacific as (
+    select
+        time, -- original column name is 'time' from the usgs api
+        magnitude,
+        depth_km,
+        place,
+        convert_timezone('UTC', 'America/Los_Angeles', time) as pacific_timestamp -- chose pacific time since all locations are west coast
+
+    from source
+),
+
+
+cleaned as (
+    select
+        date(time) as earthquake_date,
+        to_char(pacific_timestamp, 'HH12:MI:SS AM') as earthquake_time_pacific, 
+        magnitude,
+        depth_km,
+        regexp_substr(place, 'of (.*)', 1, 1, 'i', 1) as nearest_municipality
+    
+    from convert_to_pacific
+)
+
+select * from cleaned
